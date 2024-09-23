deddb6dcf9ece9270bfadf3d59ec137f1fbf78d8
Abdulkadir Havranlıoğlu <153865169+abdulkadirhavran@users.noreply.github.com>
2024-09-23 10:24:24 +0300

    updates

--- FrontEndExercises/JavaScriptMiniProjects/FactorielCalc/scriptFC.js
+++
@@ -1,7 +0,0 @@
-let facNum = +prompt("Lütfen bir sayı giriniz:");
-let result = 1;
-for (let i = 1; i <= facNum; i++) {
-  result *= i;
-}
-
-alert(`Sonuç : ${result}`);
--- FrontEndExercises/JavaScriptMiniProjects/multiplacationTable/scriptMT.js
+++
@@ -1,9 +0,0 @@
-// Çarpım tablosu
-
-for (let i = 0; i <= 11; i++) { // İlk döngü
-  if (i == 11) break; //i 11'e eşitlendiğinde döngü kırıldı.
-  for (let z = 0; z <= 15; z++) {
-    console.log(`${i} x ${z} işlem sonucu:` + i * z);
-  }
-  console.log("------------------------------");
-}
